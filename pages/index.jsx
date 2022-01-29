f511358f9065afb3213c8deb2ede9ab1711f553c
unknown <theishaansrv123@gmail.com>
2022-01-29 21:46:20 +0530

    Update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,17 @@
 import { PureComponent } from "react";
-import Link from "next/link"
+import Head from "next/head";
 
-export default class Home extends PureComponent{
+export default class Home extends PureComponent {
   render() {
-  return (
-    <div className="bg-body flex items-center justify-center w-screen h-[80vh]">
-      <div className="flex select-none text-gray-200 text-3xl">I'm a loli, <div className="ml-2 text-pred">yes.</div></div>
-    </div>
-  )
+    return (
+      <>
+        <Head>
+          <title>Altina</title>
+        </Head>
+        <div className="bg-body flex items-center justify-center w-screen h-[80vh]">
+          <div className="flex select-none text-gray-200 text-3xl">I&apos;m a loli, <div className="ml-2 text-pred">yes.</div></div>
+        </div>
+      </>
+    )
   }
 }
