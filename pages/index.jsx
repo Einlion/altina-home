a380693ce9d487f387500301cfc30bbb6ecfb9e1
unknown <theishaansrv123@gmail.com>
2022-02-14 10:47:45 +0530

    bleh

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ export default class Home extends PureComponent {
     return (
       <>
         <Head>
-          <title>Altina</title>
+          <title>Renne</title>
         </Head>
         <div className="bg-body flex items-center justify-center w-screen h-[80vh]">
           <div className="flex select-none text-gray-200 text-3xl">I&apos;m a loli, <div className="ml-2 text-pred">yes.</div></div>
